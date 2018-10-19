ad8bb014836c75a44ce291fd085a5d35cdb846e4
hejiaxu7146 <35708538+hejiaxu7146@users.noreply.github.com>
2018-10-19 03:04:17 -0400

    checks

--- Part1/code/Graph26.py
+++ Part1/code/Graph26.py
@@ -9,30 +9,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-music = pd.read_csv("music_lyrics.csv")
-#music.head()
-d11 = music["date"]
-music["right_date"] = pd.to_datetime(d11)
-
-dfnew1 = pd.DataFrame()
-dfnew1["Relatively Time"] = music["right_date"]
-dfnew1["Weeks"] = music['total weeks']
-dfnew1 = dfnew1.groupby("Relatively Time").sum()
-x11 = dfnew1.index
-x22 = dfnew1["Weeks"]
-
-test = pd.read_csv("Twitter_data_with_sentiment_score.csv")
-d = test["date"]
-test["juest_date"] = pd.to_datetime(d)
-d2 = test["juest_date"]
-test["final_date"] = d2.dt.date
-test["hot"] = test.retweets + test.favorites
-dfnew1 = pd.DataFrame()
-dfnew1["date"] = test["final_date"]
-dfnew1["retweets"] = test["retweets"]
-dfnew1["favorites"] = test["favorites"]
-dfnew1 = dfnew1.groupby("date").sum()
-
 music = pd.read_csv("lyrics_sentiment_no_lyrics.csv")
 #music.head()
 d11 = music["date"]
